1027c54b9ce9c0bedf67b5edeade75783524dd07
Eleven Pearl <tanishmehta1981@gmail.com>
2025-12-30 17:05:11 +0530

    Revise product list in script.js
    Updated product list with new items and descriptions.

--- script.js
+++ script.js
@@ -2,23 +2,305 @@ let productCount = 0;
 
 const products = [
   {
-    name: "Magnetic Surface Light",
-    description: "High-quality magnetic surface track light, 10W.",
-    rate: 350,
-    image: "images/2.png"
-  },
+  name: "6 head lenia",
+  description: "6 head lenia",
+  rate: 350,
+  image: "images/6 head lenia.png"
+},
+{
+  name: "EP 19",
+  description: "EP 19",
+  rate: 350,
+  image: "images/EP 19.png"
+},
+{
+  name: "EP 19",
+  description: "EP 19",
+  rate: 350,
+  image: "images/EP 19.png"
+},
+{
+  name: "EP 24",
+  description: "EP 24",
+  rate: 350,
+  image: "images/EP 24.png"
+},
+{
+  name: "EP 126",
+  description: "EP 126",
+  rate: 350,
+  image: "images/EP 126.png"
+},
+{
+  name: "EP 126",
+  description: "EP 126",
+  rate: 350,
+  image: "images/EP 126.png"
+},
+{
+  name: "EP 126",
+  description: "EP 126",
+  rate: 350,
+  image: "images/EP 126.png"
+},
+{
+  name: "EP 204",
+  description: "EP 204",
+  rate: 350,
+  image: "images/EP 204.png"
+},
+{
+  name: "EP 207",
+  description: "EP 207",
+  rate: 350,
+  image: "images/EP 207.png"
+},
+{
+  name: "EP 207",
+  description: "EP 207",
+  rate: 350,
+  image: "images/EP 207.png"
+},
+{
+  name: "EP 207",
+  description: "EP 207",
+  rate: 350,
+  image: "images/EP 207.png"
+},
+{
+  name: "EP 289",
+  description: "EP 289",
+  rate: 350,
+  image: "images/EP 289.png"
+},
+{
+  name: "EP 289",
+  description: "EP 289",
+  rate: 350,
+  image: "images/EP 289.png"
+},
+{
+  name: "EP 0333",
+  description: "EP 0333",
+  rate: 350,
+  image: "images/EP 0333.png"
+},
+{
+  name: "EP 0333",
+  description: "EP 0333",
+  rate: 350,
+  image: "images/EP 0333.png"
+},
+{
+  name: "EP 0333",
+  description: "EP 0333",
+  rate: 350,
+  image: "images/EP 0333.png"
+},
+{
+  name: "EP 411",
+  description: "EP 411",
+  rate: 350,
+  image: "images/EP 411.png"
+},
+{
+  name: "EP 413",
+  description: "EP 413",
+  rate: 350,
+  image: "images/EP 413.png"
+},
+{
+  name: "EP 415",
+  description: "EP 415",
+  rate: 350,
+  image: "images/EP 415.png"
+},
+{
+  name: "EP 417",
+  description: "EP 417",
+  rate: 350,
+  image: "images/EP 417.png"
+},
+{
+  name: "EP 437",
+  description: "EP 437",
+  rate: 350,
+  image: "images/EP 437.png"
+},
+{
+  name: "EP 439",
+  description: "EP 439",
+  rate: 350,
+  image: "images/EP 439.png"
+},
+{
+  name: "EP 441",
+  description: "EP 441",
+  rate: 350,
+  image: "images/EP 441.png"
+},
+{
+  name: "EP 441",
+  description: "EP 441",
+  rate: 350,
+  image: "images/EP 441.png"
+},
+{
+  name: "EP 441",
+  description: "EP 441",
+  rate: 350,
+  image: "images/EP 441.png"
+},
+{
+  name: "EP 504",
+  description: "EP 504",
+  rate: 350,
+  image: "images/EP 504.png"
+},
+{
+  name: "EP 702",
+  description: "EP 702",
+  rate: 350,
+  image: "images/EP 702.png"
+},
+{
+  name: "EP 704",
+  description: "EP 704",
+  rate: 350,
+  image: "images/EP 704.png"
+},
+{
+  name: "EP 828",
+  description: "EP 828",
+  rate: 350,
+  image: "images/EP 828.png"
+},
+{
+  name: "EP 983",
+  description: "EP 983",
+  rate: 350,
+  image: "images/EP 983.png"
+},
+{
+  name: "EP 996",
+  description: "EP 996",
+  rate: 350,
+  image: "images/EP 996.png"
+},
+{
+  name: "EP 1008",
+  description: "EP 1008",
+  rate: 350,
+  image: "images/EP 1008.png"
+},
+{
+  name: "EP 1011",
+  description: "EP 1011",
+  rate: 350,
+  image: "images/EP 1011.png"
+},
+{
+  name: "EP 1030",
+  description: "EP 1030",
+  rate: 350,
+  image: "images/EP 1030.png"
+},
+{
+  name: "EP 1053",
+  description: "EP 1053",
+  rate: 350,
+  image: "images/EP 1053.png"
+},
+{
+  name: "EP 1064",
+  description: "EP 1064",
+  rate: 350,
+  image: "images/EP 1064.png"
+},
+{
+  name: "EP 1084",
+  description: "EP 1084",
+  rate: 350,
+  image: "images/EP 1084.png"
+},
+{
+  name: "EP 1124",
+  description: "EP 1124",
+  rate: 350,
+  image: "images/EP 1124.png"
+},
+{
+  name: "EP 2007 5 HEAD",
+  description: "EP 2007 5 HEAD",
+  rate: 350,
+  image: "images/EP 2007 5 HEAD.png"
+},
+{
+  name: "EP 2007 10 HEAD",
+  description: "EP 2007 10 HEAD",
+  rate: 350,
+  image: "images/EP 2007 10 HEAD.png"
+},
+{
+  name: "EP 3006",
+  description: "EP 3006",
+  rate: 350,
+  image: "images/EP 3006.png"
+},
+{
+  name: "EP 3060-1",
+  description: "EP 3060-1",
+  rate: 350,
+  image: "images/EP 3060-1.png"
+},
+{
+  name: "EP 3060-2",
+  description: "EP 3060-2",
+  rate: 350,
+  image: "images/EP 3060-2.png"
+},
+{
+  name: "EP HONEYCOMB",
+  description: "EP HONEYCOMB",
+  rate: 350,
+  image: "images/EP HONEYCOMB.png"
+},
+{
+  name: "METAL PANEL",
+  description: "METAL PANEL",
+  rate: 350,
+  image: "images/METAL PANEL.png"
+},
+{
+  name: "METAL SURFACE",
+  description: "METAL SURFACE",
+  rate: 350,
+  image: "images/METAL SURFACE.png"
+},
+{
+  name: "PC PANEL",
+  description: "PC PANEL",
+  rate: 350,
+  image: "images/PC PANEL.png"
+},
+{
+  name: "PC SURFACE",
+  description: "PC SURFACE",
+  rate: 350,
+  image: "images/PC SURFACE.png"
+},
+{
+  name: "CURTAIN STRIP",
+  description: "CURTAIN STRIP",
+  rate: 350,
+  image: "images/CURTAIN STRIP.png"
+},
   {
-    name: "Profile",
-    description: "write here",
-    rate: 600,
-    image: "images/IMG_2259.jpeg"
-  },
-  {
-    name: "Linear Light",
-    description: "Slim linear profile light for ambient lighting and clean aesthetics.",
-    rate: 400,
-    image: "images/linear-light.jpg"
-  }
+  name: "TRACK LIGHT",
+  description: "TRACK LIGHT",
+  rate: 350,
+  image: "images/TRACK LIGHT.png"
+}
 ];
 
 function addProduct() {
@@ -210,6 +492,7 @@ function saveQuotation() {
   URL.revokeObjectURL(url);
   alert("Quotation saved successfully!");
 }
+
 
 
 
